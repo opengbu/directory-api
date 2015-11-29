1bf04ddacfd05dff0b49e0b4240e844ffdd6de3f
awasthi <awasthi@psit.in>
2015-11-29 21:44:58 +0530

    Initial commit.

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * DB configuration variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "111111");
+define("DB_DATABASE", "faculty");
+?>
---
+++ getusers.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Creates Unsynced rows data as JSON
+ */
+    include_once 'db_functions.php';
+    $db = new DB_Functions();
+    $users = $db->getUnSyncRowCount();
+    $a = array();
+    $b = array();
+    if ($users != false){
+        while ($row = mysql_fetch_array($users)) {      
+            $b["userId"] = $row["Id"];
+            $b["userName"] = $row["Name"];
+			$b["userPhone"] = $row["Phone"];
+            array_push($a,$b);
+        }
+        echo json_encode($a);
+    }
+?>
